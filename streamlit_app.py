f99416e9ab17b3bbc96e2d1c28eaaffbcc9761bf
pythonlearning17 <107101839+pythonlearning17@users.noreply.github.com>
2022-12-16 15:25:23 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner') 
+
+streamlit.header('Breakfast Menu')
+streamlit.text(' blueberry oatmeal')
+streamlit.text(' strawberry oatmeal')
+streamlit.text(' kale smoothie')
