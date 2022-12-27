af58835d0a987a026534989860d0d7fd9bf6dcf7
pythonlearning17 <107101839+pythonlearning17@users.noreply.github.com>
2022-12-27 10:40:57 +0530

    Update streamlit_app.py
    get_fruit_load list function added and a button to trigger it

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,12 +51,23 @@ streamlit.stop()
 
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall() #fetching all rows
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) # moved inside func if 
+#my_cur = my_cnx.cursor() # same as my_cnx.cursor() as my_cur in function
+#my_cur.execute("select * from fruit_load_list") #-- moved
+#my_data_rows = my_cur.fetchall() #fetching all rows #--moved
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#snowflaker relted function 
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+    
+    
+#adding a button to load the fruit     
+if streamlit.button('Get Fruit Load List')
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 # Adding code for challange lab 
 #adding text field to allow end user to add fruit 
